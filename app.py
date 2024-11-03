e8d0b1cb863f74cc3534b8b18a98b23fd8fddb5b
liamjosephnolan <liamjosephnolan@gmail.com>
2024-11-03 11:25:19 +0100

    Debug: CORS origin testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS  # Import CORSapp = Flask(__name__)
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://liamjosephnolan.com"}})  # Modify "*" to your website URL if possible
+CORS(app)
 
 @app.route('/api/average_capacity', methods=['GET'])
 def average_capacity():
